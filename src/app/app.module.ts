3541d81b0dcadb336ee5da22a6971947e4721247
Ana Maria Mitrea <72494328+AnaMitrea@users.noreply.github.com>
2023-02-21 20:07:47 +0200

    Feature/landing forms (#2)
    * Landing page
    
    * Landing page
    
    * Routing fix
    
    * Routing fix
    
    * .
    
    * Login + Register containers, generated default services
    
    * Logo on Landing Page
    
    * Refactor structure
    
    * Refactor structure
    
    * shared module error
    
    * home
    
    * side nav bar incomplete
    
    * Routing fix with hash
    
    ---------
    
    Co-authored-by: anstoian <113590500+anmitrea@users.noreply.github.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { SharedModule } from "./shared/shared.module";
+
 
 @NgModule({
   declarations: [
@@ -16,7 +19,10 @@ import { SharedModule } from "./shared/shared.module";
     BrowserAnimationsModule,
     SharedModule
   ],
-  providers: [],
+  providers: [{
+    provide: LocationStrategy,
+    useClass: HashLocationStrategy
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
