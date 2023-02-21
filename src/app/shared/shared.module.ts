b49b3179971b0bee5e1e9eb966625ece1c692d60
Ana Maria Mitrea <anamitrea27@gmail.com>
2023-02-21 23:14:55 +0200

    delete unused

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,11 +4,9 @@ import { TopBarComponent } from './components/top-bar/top-bar.component';
 import { SideNavbarComponent } from "./components/side-navbar/side-navbar.component";
 import { RouterModule } from '@angular/router';
 import { MatIconModule } from "@angular/material/icon";
-import { IconComponent } from './components/icon/icon.component';
 import { FlexModule } from 'ngx-flexible-layout';
 import { MatDividerModule } from '@angular/material/divider';
 
-// declarations and exports should be the same (=what this modules export when it is imported in another module)
 @NgModule({
   imports: [
     CommonModule,
@@ -20,7 +18,6 @@ import { MatDividerModule } from '@angular/material/divider';
   declarations: [
     TopBarComponent,
     SideNavbarComponent,
-    IconComponent,
   ],
   exports: [
     TopBarComponent,
