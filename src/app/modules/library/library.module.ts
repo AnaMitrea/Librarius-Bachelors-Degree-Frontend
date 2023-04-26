0c09e3f7dc4a4df71de639a53323d0eba7271abd
AnaMitrea <anamitrea27@gmail.com>
2023-04-26 17:42:16 +0300

    short url

--- src/app/modules/library/library.module.ts
+++ src/app/modules/library/library.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { LibraryRoutingModule } from './library-routing.module';
 import { ContainerComponent } from './components/container/container.component';
-import { SharedModule } from '../../shared/shared.module';
+import { SharedModule } from '@app-shared/shared.module';
 import { ReadingSectionComponent } from './components/reading-section/reading-section.component';
 import { FlexModule } from 'ngx-flexible-layout';
 import { MatIconModule } from '@angular/material/icon';
