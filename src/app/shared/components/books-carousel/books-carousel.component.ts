3848a9af46b199dbbcbfc9b7dae9e49b7a98274a
AnaMitrea <anamitrea27@gmail.com>
2023-05-20 20:14:24 +0300

    Refactor Home page clickable author name

--- src/app/shared/components/books-carousel/books-carousel.component.ts
+++ src/app/shared/components/books-carousel/books-carousel.component.ts
@@ -2,6 +2,8 @@ import { Component, Input } from '@angular/core';
 import { OwlOptions } from 'ngx-owl-carousel-o';
 import { Router } from '@angular/router';
 import {Book} from "@app-modules/home/shared/models";
+import {LIBRARY_AUTHOR_ROUTE, LIBRARY_BOOK_ROUTE} from "@app-utils/constants";
+import {processAuthorName} from "@app-utils/data-transformers";
 
 @Component({
   selector: 'app-books-carousel',
@@ -42,6 +44,14 @@ export class BooksCarouselComponent {
   constructor(private router: Router) { }
 
   redirectToBookUrl(id: string) {
-    this.router.navigate(['/library/book', id]).then();
+    this.router.navigate([LIBRARY_BOOK_ROUTE, id]).then();
+  }
+
+  onAuthorClick(id: number) {
+    this.router.navigate([LIBRARY_AUTHOR_ROUTE, id]).then();
+  }
+
+  processName(author: string): string {
+    return processAuthorName(author);
   }
 }
